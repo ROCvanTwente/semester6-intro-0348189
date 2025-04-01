8194a0a044e778cfdfc4e96f2f43f62b4576eb29
Senna <127939389+0348189@users.noreply.github.com>
2025-04-01 09:43:39 +0200

    Je kunt via de program.cs info in de database doen. de creditcard info word dan geïncrypt

--- ASPSEC2/ASPSEC2/Program.cs
+++ ASPSEC2/ASPSEC2/Program.cs
@@ -1,64 +1,69 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
-namespace ASPSEC2
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        string connectionString = "Server=.;Database=ASPSEC2;Trusted_Connection=True;Encrypt=False";
+        string firstName = "John";
+        string lastName = "Doe";
+        string street = "Main St";
+        string houseNumber = "123";
+        string postalCode = "12345";
+        string city = "Hometown";
+        string creditCardNumber = "1234-5678-9012-3456";
+        string key = "0123456789abcdef"; // 16 bytes key for AES-128
+
+        string encryptedCreditCardNumber = Encrypt(creditCardNumber, key);
+
+        SavePersonData(connectionString, firstName, lastName, street, houseNumber, postalCode, city, encryptedCreditCardNumber);
+    }
+
+    static string Encrypt(string plainText, string key)
+    {
+        using (Aes aes = Aes.Create())
         {
-            string creditCardNumber = "1234-5678-9012-3456";
-            string key = "0123456789abcdef"; // 16 bytes key for AES-128
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16]; // Initialization vector with 16 bytes of zeros
 
-            string encrypted = Encrypt(creditCardNumber, key);
-            Console.WriteLine($"Encrypted: {encrypted}");
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 
-            string decrypted = Decrypt(encrypted, key);
-            Console.WriteLine($"Decrypted: {decrypted}");
-        }
-
-        static string Encrypt(string plainText, string key)
-        {
-            using (Aes aes = Aes.Create())
+            using (MemoryStream ms = new MemoryStream())
             {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = new byte[16]; // Initialization vector with 16 bytes of zeros
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter sw = new StreamWriter(cs))
                     {
-                        using (StreamWriter sw = new StreamWriter(cs))
-                        {
-                            sw.Write(plainText);
-                        }
+                        sw.Write(plainText);
                     }
-                    return Convert.ToBase64String(ms.ToArray());
                 }
+                return Convert.ToBase64String(ms.ToArray());
             }
         }
+    }
 
-        static string Decrypt(string cipherText, string key)
+    static void SavePersonData(string connectionString, string firstName, string lastName, string street, string houseNumber, string postalCode, string city, string encryptedCreditCardNumber)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            using (Aes aes = Aes.Create())
+            connection.Open();
+            string query = "INSERT INTO Persons (FirstName, LastName, Street, HouseNumber, PostalCode, City, CreditCardNumber) VALUES (@FirstName, @LastName, @Street, @HouseNumber, @PostalCode, @City, @CreditCardNumber)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = new byte[16]; // Initialization vector with 16 bytes of zeros
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                command.Parameters.AddWithValue("@Street", street);
+                command.Parameters.AddWithValue("@HouseNumber", houseNumber);
+                command.Parameters.AddWithValue("@PostalCode", postalCode);
+                command.Parameters.AddWithValue("@City", city);
+                command.Parameters.AddWithValue("@CreditCardNumber", encryptedCreditCardNumber);
 
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(cipherText)))
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader sr = new StreamReader(cs))
-                        {
-                            return sr.ReadToEnd();
-                        }
-                    }
-                }
+                command.ExecuteNonQuery();
             }
         }
     }
